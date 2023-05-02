a0ccc7f9654498dcca20ac07aba6fa256863de5f
Holden Cormier <90851068+holden-cormier@users.noreply.github.com>
2023-05-02 11:39:40 -0400

    Update VSDS_Queries.sql

--- DB/VSDS_Queries.sql
+++ DB/VSDS_Queries.sql
@@ -1,4 +1,18 @@
 
 -- get all projects
+select * from projects;
 
-select * from projects 
+-- get all members
+select * from gen_member;
+
+-- get all exec members
+select * from exec_board;
+
+-- get all presidents
+select * from president;
+
+-- get all managing info
+select * from manages;
+
+-- get all issues
+select * from issues;
